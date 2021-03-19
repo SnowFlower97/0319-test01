fa018ae7fb1a15b4a0cf737b73c98110e6781021
SnowFlower97 <80624927+SnowFlower97@users.noreply.github.com>
2021-03-19 18:35:49 +0900

    ddd

---
+++ board/src/test/java/com/boardtest01/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardtest01.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
